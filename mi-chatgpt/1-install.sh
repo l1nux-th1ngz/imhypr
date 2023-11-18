50adf0265d198c22738b35e428a36a164c98aca0
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-11-17 19:24:58 -0700

    Add files via upload

---
+++ mi-chatgpt/1-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# ------------------------------------------------------
+# Install Script for MyChatGpt
+# ------------------------------------------------------
+
+pip install openai
+pip install pyperclip
+pip install pyaml
+pip install click
+pip install rich
